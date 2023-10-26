569447e2594dfd9dd9816f02842ef3deb0345b58
C. Graamans <graamans@gmail.com>
2023-10-26 23:37:21 +0200

    Cleaned deploy-commands

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,64 +1,14 @@
-// import * as fs from 'fs';
-// import {REST} from '@discordjs/rest';
-// import {Routes} from 'discord-api-types/v9';
-// import discord from "./src/services/discord";
-
-// const commands = [];
-
-// const commandFiles = fs.readdirSync('./src/commands').filter(file=>!file.endsWith(".map"));
-
-// // Place your client and guild ids here
-
-// Eurobot
-const clientId = '673969077319892992';
-
-// // EucoBot
-// // const clientId = '736008030532927599';
-
-// FG
-const guildId = '257838262943481857';
-
-// // EP
-// // const guildId = '637843781617713172';
-
-// // FG-E
-// // const guildId = '633016043136090124';
-
-// for (const file of commandFiles) {
-// 	const command = require(`./src/commands/${file}`);
-// 	commands.push(command.data.toJSON());
-// }
-
-// const rest = new REST({ version: '9' }).setToken(process.env["EUROBOT_DISCORD"]);
-
-// (async () => {
-// 	try {
-// 		console.log('Started refreshing application (/) commands.');
-
-// 		await rest.put(
-// 			Routes.applicationCommands(clientId),
-// 			{ body: commands },
-// 		);		
-
-// 		// await rest.put(
-// 		// 	Routes.applicationGuildCommands(clientId, guildId),
-// 		// 	{ body: commands },
-// 		// );
-
-// 		console.log('Successfully reloaded application (/) commands.');
-// 	} catch (error) {
-// 		console.error(error);
-// 	}
-
-// })();
-
-
-
-
 import { REST, Routes } from 'discord.js';
 // import { clientId, guildId, token } from './config.json';
 import fs from 'fs';
 import path from 'path';
+
+
+// Eurobot
+const clientId = '673969077319892992';
+
+// FG
+const guildId = '257838262943481857';
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
